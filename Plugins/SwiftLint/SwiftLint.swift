b9ebc1490c93532a3639520b0bc23d478bfe06c4
Marc Schultz <lunij@users.noreply.github.com>
2023-01-18 17:21:53 +0100

    No cache at all

--- Plugins/SwiftLint/SwiftLint.swift
+++ Plugins/SwiftLint/SwiftLint.swift
@@ -4,15 +4,15 @@ import PackagePlugin
 @main
 struct SwiftLint: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
-        let parameters = Environment.isCachingDisabled ? ["--no-cache"] : ["--cache-path", context.pluginWorkDirectory.string]
         return [
             .buildCommand(
                 displayName: "Running SwiftLint for \(target.name)",
                 executable: try context.tool(named: "swiftlint").path,
                 arguments: [
                     "lint",
+                    "--no-cache",
                     target.directory.string
-                ] + parameters,
+                ],
                 environment: [:]
             )
         ]
@@ -24,29 +24,18 @@ import XcodeProjectPlugin
 
 extension SwiftLint: XcodeBuildToolPlugin {
     func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
-        let parameters = Environment.isCachingDisabled ? ["--no-cache"] : ["--cache-path", context.pluginWorkDirectory.string]
         return [
             .buildCommand(
                 displayName: "Running SwiftLint for \(target.displayName)",
                 executable: try context.tool(named: "swiftlint").path,
                 arguments: [
                     "lint",
+                    "--no-cache",
                     context.xcodeProject.directory.string
-                ] + parameters,
+                ],
                 environment: [:]
             )
         ]
     }
 }
 #endif
-
-import Foundation
-
-enum Environment {
-    static var isCachingDisabled: Bool {
-        if let _ = ProcessInfo.processInfo.environment["SWIFTLINT_NO_CACHE"] {
-            return true
-        }
-        return false
-    }
-}
