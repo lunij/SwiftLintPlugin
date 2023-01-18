23e0a996e331ede740fbb9dc820f496dc7cf05db
Marc Schultz <lunij@users.noreply.github.com>
2023-01-18 12:57:26 +0100

    Disable caching

--- Plugins/SwiftLint/SwiftLint.swift
+++ Plugins/SwiftLint/SwiftLint.swift
@@ -4,16 +4,15 @@ import PackagePlugin
 @main
 struct SwiftLint: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        let parameters = Environment.isCachingDisabled ? ["--no-cache"] : ["--cache-path", context.pluginWorkDirectory.string]
         return [
             .buildCommand(
                 displayName: "Running SwiftLint for \(target.name)",
                 executable: try context.tool(named: "swiftlint").path,
                 arguments: [
                     "lint",
-                    "--cache-path",
-                    context.pluginWorkDirectory.string,
                     target.directory.string
-                ],
+                ] + parameters,
                 environment: [:]
             )
         ]
@@ -25,19 +24,29 @@ import XcodeProjectPlugin
 
 extension SwiftLint: XcodeBuildToolPlugin {
     func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
+        let parameters = Environment.isCachingDisabled ? ["--no-cache"] : ["--cache-path", context.pluginWorkDirectory.string]
         return [
             .buildCommand(
                 displayName: "Running SwiftLint for \(target.displayName)",
                 executable: try context.tool(named: "swiftlint").path,
                 arguments: [
                     "lint",
-                    "--cache-path",
-                    "\(context.pluginWorkDirectory.string)",
                     context.xcodeProject.directory.string
-                ],
+                ] + parameters,
                 environment: [:]
             )
         ]
     }
 }
 #endif
+
+import Foundation
+
+enum Environment {
+    static var isCachingDisabled: Bool {
+        if let _ = ProcessInfo.processInfo.environment["SWIFTLINT_NO_CACHE"] {
+            return true
+        }
+        return false
+    }
+}
